25cc0c119e4f38975fc723bd75bf5a8e4da537e8
Antoine GUÉRON <agueron@a5sys.com>
2018-12-05 03:23:53 -0800

    exercice angular openclassromm

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'lorem-blog';
+  posts = [
+    {
+      title: 'Post #1',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a ante tortor. Maecenas malesuada nunc efficitur metus ' +
+        'lacinia, sed dignissim purus porttitor. Vivamus suscipit nisl sit amet augue vehicula, nec cursus tortor eleifend. Nunc ' +
+        'condimentum in nibh at ornare. Pellentesque elementum bibendum ullamcorper. Aliquam placerat nisi ac consequat malesuada.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Poste #2',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a ante tortor. Maecenas malesuada nunc efficitur metus ' +
+        'lacinia, sed dignissim purus porttitor. Vivamus suscipit nisl sit amet augue vehicula, nec cursus tortor eleifend. Nunc ' +
+        'condimentum in nibh at ornare. Pellentesque elementum bibendum ullamcorper. Aliquam placerat nisi ac consequat malesuada.',
+      loveIts: 0,
+      created_at: new Date()
+    },
+    {
+      title: 'Poste #3',
+      content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed a ante tortor. Maecenas malesuada nunc efficitur metus ' +
+        'lacinia, sed dignissim purus porttitor. Vivamus suscipit nisl sit amet augue vehicula, nec cursus tortor eleifend. Nunc ' +
+        'condimentum in nibh at ornare. Pellentesque elementum bibendum ullamcorper. Aliquam placerat nisi ac consequat malesuada.',
+      loveIts: 0,
+      created_at: new Date()
+    }
+  ];
 }
