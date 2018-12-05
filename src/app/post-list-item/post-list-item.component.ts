3c822600f67c8fc0cbf011e2f3e874e6562aceac
Antoine GUÉRON <agueron@a5sys.com>
2018-12-05 03:29:16 -0800

    Add components

---
+++ src/app/post-list-item/post-list-item.component.ts
@@ -0,0 +1,32 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-post-list-item',
+  templateUrl: './post-list-item.component.html',
+  styleUrls: ['./post-list-item.component.scss']
+})
+export class PostListItemComponent implements OnInit {
+  @Input() title: string;
+  @Input() content: string;
+  @Input() loveIts: number;
+  @Input() createdAt: Date;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getColor() {
+    if (this.loveIts > 0) {
+      return 'green';
+    } else if (this.loveIts < 0) {
+      return 'red';
+    }
+  }
+  AddLoveIt() {
+    this.loveIts = this.loveIts + 1;
+  }
+  RemoveLoveIt() {
+    this.loveIts = this.loveIts - 1;
+  }
+}
